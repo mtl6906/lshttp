0a3efdaee84960be24d7fd0fc063823ecceb218a
mtl6906 <zzz5486129@gmail.com>
2021-09-18 16:05:04 +0800

    modify QueryString

--- include/ls/http/QueryString.h
+++ include/ls/http/QueryString.h
@@ -16,6 +16,8 @@ namespace ls
 				int lengthOfString() override;
 				std::string encode(const std::string &text);
 				std::string decode(const std::string &text);
+				std::string getParameter(const std::string &key);
+				void setParameter(const std::string &key, const std::string &value);
 			protected:
 				void parse(const std::string &text) override;
 				OrderedMap<std::string, std::string> om;
